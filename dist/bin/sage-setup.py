3a708fc2acb0a1c1b102362004ef519e46d48938
Felix Salfelder <felix@em.cs.uni-frankfurt.de>
2013-06-23 12:02:45 +0200

    toplevel build system continued
    - package-(un)install
    - TODO
    - dist environment
    - sage-dist-install program
    - sage-setup.py wrapper

---
+++ dist/bin/sage-setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+# this program is intended to be called instead of "python setup.py"
+# see #14804
+
+# FIXME: too ugly
+
+import distutils.file_util, distutils.dir_util, os
+import distutils.command.install
+#import distutils.util
+distcopy = distutils.file_util.copy_file
+distmkdir = distutils.dir_util.mkpath
+distbytecompile = distutils.util.byte_compile
+
+
+destdir = ""
+
+try:
+	destdir = os.environ["DESTDIR"]
+except:
+	pass
+
+def ownmkdir(*args, **kwds):
+	if args[0][0] == "/":
+		print "dir", destdir + args[0]
+		args = ( destdir + args[0], )
+	return distmkdir(*args, **kwds)
+
+def ownbytecompile(*args, **kwds):
+	newargs=[]
+	for i in args[0]:
+		if i[0] == "/":
+			i = destdir + i
+		newargs.append(i)
+	return distbytecompile(newargs, **kwds)
+
+def myowncopy(*args, **kwds):
+	# preserve_mode=1, preserve_times=1, update=0, link=None, verbose=1, dry_run=0
+	update = kwds.get("update",0)
+	if args[0][0] == "/":
+		args = ( destdir + args[0], args[1] )
+	if args[1][0] == "/":
+		args = ( args[0], destdir + args[1] )
+
+	print "call", args
+	a = distcopy(*args, **kwds)
+	print "done"
+	return a
+
+distutils.file_util.copy_file = myowncopy
+distutils.dir_util.mkpath = ownmkdir
+distutils.util.byte_compile = ownbytecompile
+
+import sys
+sys.argv[0] = "setup.py"
+c = os.getcwd()
+s = os.path.join(c, "setup.py")
+sys.path.append(os.getcwd())
+execfile("setup.py")
